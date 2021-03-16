78bba90398c55649b19e1e704bc2da472a48401b
MR_QIN <3066004354@qq.com>
2021-03-16 18:52:51 +0800

    JAVA

--- kxxxl/src/main/java/com/example/kxxxl/MainActivity.java
+++ kxxxl/src/main/java/com/example/kxxxl/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.kxxxl;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button clickBtu;
+    private TextView tx1;
+    private int n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tx1 = (TextView)findViewById(R.id.textView) ;
+        clickBtu = (Button)findViewById(R.id.but_demo);
+        clickBtu.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.but_demo:
+                n++;
+                tx1.setText("n:"+n);
+                break;
+        }
     }
 }
